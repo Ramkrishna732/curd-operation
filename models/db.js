82e895c6c8ef35c4595ea520919ce62226426ee3
Ramkrishna732 <ramkrishnamaurya732@gmail.com>
2022-01-26 18:33:39 +0530

    bug in clouddb

--- models/db.js
+++ models/db.js
@@ -1,10 +1,15 @@
-const mongoose = require('mongoose');
-require('./employee.model');
-const url = process.env.MONGODB_LOCAL||process.env.MONGODB_CLOUD
+const mongoose = require("mongoose");
+require("./employee.model");
+// const url = process.env.MONGODB_LOCAL||process.env.MONGODB_CLOUD
+const url = process.env.MONGODB_CLOUD;
 
-mongoose.connect(url,{useNewUrlParser:true},(err) => {
-    if(!err){ console.log("MongoDB Connection Succeeded");}
-    else{
-        console.log("An Error Occured");
-    } 
+mongoose.connect(url, {
+    useNewUrlParser: true,
+   // useFindAndModify: false,
+    useUnifiedTopology: true
+  
+}).then(()=>{
+    console.log("db connected success");
+}).catch((err)=>{
+    console.log(err);
 })
