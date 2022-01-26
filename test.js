68e4586b9e7dc297bf9a8da5ffbc5ff16759c99b
Ramkrishna732 <ramkrishnamaurya732@gmail.com>
2022-01-26 16:20:10 +0530

    please lord help me

--- test.js
+++ test.js
@@ -1,9 +1,24 @@
 const express = require("express");
 var app = express();
+app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res)=>{
-    res.send('testing deployement...')
+    res.send('testing deployement...');
+    res.end();
 })
-app.listen(5000, () => {
+// custom 404 page
+app.use((req, res) => {
+    res.status(404);
+    res.render('404')
+});
+
+// custom 500 page
+app.use((err, req, res, nex) => {
+    console.err(err.stack);
+    res.status(500);
+    res.render('500')
+});
+
+app.listen(app.get("port"), () => {
   console.log("Server is listening on Port 5000");
 });
